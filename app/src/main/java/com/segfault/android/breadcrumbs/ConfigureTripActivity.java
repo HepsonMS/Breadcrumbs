a2e1f3ddc3bc09cc25bb0d7c4ba628bc319cea49
Hepson Sanchez <hepsonms6@gmail.com>
2019-07-20 05:41:15 -0700

    Route drawing and uploading to Firebase for Traverlers

--- app/src/main/java/com/segfault/android/breadcrumbs/ConfigureTripActivity.java
+++ app/src/main/java/com/segfault/android/breadcrumbs/ConfigureTripActivity.java
@@ -2,13 +2,27 @@ package com.segfault.android.breadcrumbs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConfigureTripActivity extends AppCompatActivity {
 
+    private Button mSetRoute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configure_trip);
+
+        mSetRoute = (Button) findViewById(R.id.set_route);
+
+        mSetRoute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConfigureTripActivity.this, SetRouteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
